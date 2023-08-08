a26e54ded467302a3a3828f1f37a066ece54dc97
HenokG <heni.gebrehiwot@gmail.com>
2023-08-07 18:38:07 -0600

    Fetch Menu Items & Interfaces in TS

--- src/Components/Layout/Header.tsx
+++ src/Components/Layout/Header.tsx
@@ -21,7 +21,7 @@ function Header() {
               <div className="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul className="navbar-nav me-auto mb-2 mb-lg-0">
                   <li className="nav-item">
-                    <a className="nav-link" aria-current="page" href="#">
+                    <a className="nav-link" aria-current="page" href="https://transform.tools/json-to-typescript">
                       Home
                     </a>
                   </li>
@@ -29,7 +29,7 @@ function Header() {
                   <li className="nav-item dropdown">
                     <a
                       className="nav-link dropdown-toggle"
-                      href="#"
+                      href="https://transform.tools/json-to-typescript"
                       role="button"
                       data-bs-toggle="dropdown"
                       aria-expanded="false"
@@ -38,17 +38,17 @@ function Header() {
                     </a>
                     <ul className="dropdown-menu">
                       <li>
-                        <a className="dropdown-item" href="#">
+                        <a className="dropdown-item" href="https://transform.tools/json-to-typescript">
                           Action
                         </a>
                       </li>
                       <li>
-                        <a className="dropdown-item" href="#">
+                        <a className="dropdown-item" href="https://transform.tools/json-to-typescript">
                           Another action
                         </a>
                       </li>
                       <li>
-                        <a className="dropdown-item" href="#">
+                        <a className="dropdown-item" href="https://transform.tools/json-to-typescript">
                           Something else here
                         </a>
                       </li>
