0379acbaeef91560f50c0e34b00e1c60fba92c7b
HenokG <heni.gebrehiwot@gmail.com>
2023-08-09 17:31:58 -0600

    Loader on Add to Cart in Details Page

--- src/Pages/MenuItemDetails.tsx
+++ src/Pages/MenuItemDetails.tsx
@@ -4,7 +4,7 @@ import { useGetMenuItemByIdQuery } from "../Apis/menuItemApi";
 import { useNavigate } from "react-router-dom";
 import { useState } from "react";
 import { useUpdateShoppingCartMutation } from "../Apis/shoppingCartApi";
-import { MainLoader } from "../Components/Page/Common";
+import { MainLoader, MiniLoader } from "../Components/Page/Common";
 // USER ID-d58976a0-bab2-474c-8ce3-fd0fed12ae45===a52ddc7b-d3d6-4727-b23f-c45f495c043f
 
 function MenuItemDetails() {
@@ -35,87 +35,93 @@ function MenuItemDetails() {
   };
   return (
     <div className="container pt-4 pt-md-5">
-       {!isLoading ? (
-                <div className="row">
-                <div className="col-7">
-                  <h2 className="text-success">{data.result?.name}</h2>
-                  <span>
-                    <span
-                      className="badge text-bg-dark pt-2"
-                      style={{ height: "40px", fontSize: "20px" }}
-                    >
-                      {data.result?.category}
-                    </span>
-                  </span>
-                  <span>
-                    <span
-                      className="badge text-bg-light pt-2"
-                      style={{ height: "40px", fontSize: "20px" }}
-                    >
-                      {data.result?.specialTag}
-                    </span>
-                  </span>
-                  <p style={{ fontSize: "20px" }} className="pt-2">
-                    {data.result?.description}
-                  </p>
-                  <span className="h3">${data.result?.price}</span> &nbsp;&nbsp;&nbsp;
-                  <span
-                    className="pb-2  p-3"
-                    style={{ border: "1px solid #333", borderRadius: "30px" }}
+      {!isLoading ? (
+        <div className="row">
+          <div className="col-7">
+            <h2 className="text-success">{data.result?.name}</h2>
+            <span>
+              <span
+                className="badge text-bg-dark pt-2"
+                style={{ height: "40px", fontSize: "20px" }}
+              >
+                {data.result?.category}
+              </span>
+            </span>
+            <span>
+              <span
+                className="badge text-bg-light pt-2"
+                style={{ height: "40px", fontSize: "20px" }}
+              >
+                {data.result?.specialTag}
+              </span>
+            </span>
+            <p style={{ fontSize: "20px" }} className="pt-2">
+              {data.result?.description}
+            </p>
+            <span className="h3">${data.result?.price}</span> &nbsp;&nbsp;&nbsp;
+            <span
+              className="pb-2  p-3"
+              style={{ border: "1px solid #333", borderRadius: "30px" }}
+            >
+              <i
+                onClick={() => {
+                  handleQuantity(-1);
+                }}
+                className="bi bi-dash p-1"
+                style={{ fontSize: "25px", cursor: "pointer" }}
+              ></i>
+              <span className="h3 mt-3 px-3">{quantity}</span>
+              <i
+                onClick={() => {
+                  handleQuantity(+1);
+                }}
+                className="bi bi-plus p-1"
+                style={{ fontSize: "25px", cursor: "pointer" }}
+              ></i>
+            </span>
+            <div className="row pt-4">
+              <div className="col-5">
+                {isAddingToCart ? (
+                  <button disabled className="btn btn-success form-control">
+                    <MiniLoader />
+                  </button>
+                ) : (
+                  <button
+                    className="btn btn-success form-control"
+                    onClick={() => handleAddToCart(data.result?.id)}
                   >
-                    <i
-                    onClick={() => {
-                      handleQuantity(-1);
-                    }}
-                      className="bi bi-dash p-1"
-                      style={{ fontSize: "25px", cursor: "pointer" }}
-                    ></i>
-                    <span className="h3 mt-3 px-3">{quantity}</span>
-                    <i
-                    onClick={() => {
-                      handleQuantity(+1);
-                    }}
-                      className="bi bi-plus p-1"
-                      style={{ fontSize: "25px", cursor: "pointer" }}
-                    ></i>
-                  </span>
-                  <div className="row pt-4">
-                    <div className="col-5">
-                    <button
-                  className="btn btn-success form-control"
-                  onClick={() => handleAddToCart(data.result?.id)}
-                >
-                        Add to Cart
-                      </button>
-                    </div>
-      
-                    <div className="col-5 ">
-                      <button
-                        className="btn btn-secondary form-control"
-                        onClick={() => navigate(-1)}
-                      >
-                        Back to Home
-                      </button>
-                    </div>
-                  </div>
-                </div>
-                <div className="col-5">
-                  <img
-                    src={data.result.image}
-                    width="100%"
-                    style={{ borderRadius: "50%" }}
-                    alt="No content"
-                  ></img>
-                </div>
+                    Add to Cart
+                  </button>
+                )}
               </div>
-        ) : (
-          <div
-            className="d-flex justify-content-center"
-            style={{ width: "100%" }}
-          >
-            <MainLoader />
+
+              <div className="col-5 ">
+                <button
+                  className="btn btn-secondary form-control"
+                  onClick={() => navigate(-1)}
+                >
+                  Back to Home
+                </button>
+              </div>
+            </div>
           </div>
-        )}
+          <div className="col-5">
+            <img
+              src={data.result.image}
+              width="100%"
+              style={{ borderRadius: "50%" }}
+              alt="No content"
+            ></img>
+          </div>
+        </div>
+      ) : (
+        <div
+          className="d-flex justify-content-center"
+          style={{ width: "100%" }}
+        >
+          <MainLoader />
+        </div>
+      )}
     </div>
   );
 }
