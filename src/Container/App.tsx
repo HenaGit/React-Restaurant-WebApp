630f385f6db3c03446810f592eaa8fdc8b11ab41
HenokG <heni.gebrehiwot@gmail.com>
2023-08-11 18:55:41 -0600

    Small Bug with Authentication

--- src/Container/App.tsx
+++ src/Container/App.tsx
@@ -13,6 +13,9 @@ import { useEffect } from "react";
 import { useDispatch } from "react-redux";
 import { useGetShoppingCartQuery } from "../Apis/shoppingCartApi";
 import { setShoppingCart } from "../Storage/Redux/shoppingCartSlice";
+import jwt_decode from "jwt-decode";
+import { userModel } from "../Interfaces";
+import { setLoggedInUser } from "../Storage/Redux/userAuthSlice";
 
 function App() {
   const dispatch = useDispatch();
@@ -20,10 +23,16 @@ function App() {
   const { data, isLoading } = useGetShoppingCartQuery(
     "d58976a0-bab2-474c-8ce3-fd0fed12ae45"
   );
-
+  useEffect(() => {
+    const localToken = localStorage.getItem("token");
+    if (localToken) {
+      const { fullName, id, email, role }: userModel = jwt_decode(localToken);
+      dispatch(setLoggedInUser({ fullName, id, email, role }));
+    }
+  }, []);
   useEffect(() => {
     if (!isLoading) {
-      console.log(data.result);
+      //console.log(data.result);
       dispatch(setShoppingCart(data.result?.cartItems));
     }
   }, [data]);
