701b105024239439096eecc5520375f7cdf5e074
HenokG <heni.gebrehiwot@gmail.com>
2023-08-11 18:28:41 -0600

    Login and Logout in Action

--- src/Components/Layout/Header.tsx
+++ src/Components/Layout/Header.tsx
@@ -1,17 +1,25 @@
 import React from "react";
-import { NavLink } from "react-router-dom";
+import { NavLink, useNavigate } from "react-router-dom";
 import { cartItemModel, userModel } from "../../Interfaces";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { RootState } from "../../Storage/Redux/store";
+import { emptyUserState, setLoggedInUser } from "../../Storage/Redux/userAuthSlice";
 let logo = require("../../Assets/Images/mango.png");
 
 function Header() {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
   const shoppingCartFromStore: cartItemModel[] = useSelector(
     (state: RootState) => state.shoppingCartStore.cartItems ?? []
   );
   const userData: userModel = useSelector(
     (state: RootState) => state.userAuthStore
   );
+  const handleLogout = () => {
+    localStorage.removeItem("token");
+    dispatch(setLoggedInUser({ ...emptyUserState }));
+    navigate("/");
+  };
     return (
         <div>
           <nav className="navbar navbar-expand-lg bg-dark navbar-dark">
@@ -101,6 +109,7 @@ function Header() {
                           height: "40px",
                           width: "100px",
                         }}
+                        onClick={handleLogout}
                       >
                         Logout
                       </button>
