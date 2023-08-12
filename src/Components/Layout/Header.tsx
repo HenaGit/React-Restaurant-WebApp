ce9897644eef0d7e14f65cd85d805637afd0d6a3
HenokG <heni.gebrehiwot@gmail.com>
2023-08-11 19:44:24 -0600

    Add Test Comp for Auth & Authorization

--- src/Components/Layout/Header.tsx
+++ src/Components/Layout/Header.tsx
@@ -3,7 +3,10 @@ import { NavLink, useNavigate } from "react-router-dom";
 import { cartItemModel, userModel } from "../../Interfaces";
 import { useDispatch, useSelector } from "react-redux";
 import { RootState } from "../../Storage/Redux/store";
-import { emptyUserState, setLoggedInUser } from "../../Storage/Redux/userAuthSlice";
+import {
+  emptyUserState,
+  setLoggedInUser,
+} from "../../Storage/Redux/userAuthSlice";
 let logo = require("../../Assets/Images/mango.png");
 
 function Header() {
@@ -20,28 +23,28 @@ function Header() {
     dispatch(setLoggedInUser({ ...emptyUserState }));
     navigate("/");
   };
-    return (
-        <div>
-          <nav className="navbar navbar-expand-lg bg-dark navbar-dark">
-            <div className="container-fluid">
-            <NavLink className="nav-link" aria-current="page" to="/">
+  return (
+    <div>
+      <nav className="navbar navbar-expand-lg bg-dark navbar-dark">
+        <div className="container-fluid">
+          <NavLink className="nav-link" aria-current="page" to="/">
             <img src={logo} style={{ height: "40px" }} className="m-1" />
           </NavLink>
-              <button
-                className="navbar-toggler"
-                type="button"
-                data-bs-toggle="collapse"
-                data-bs-target="#navbarSupportedContent"
-                aria-controls="navbarSupportedContent"
-                aria-expanded="false"
-                aria-label="Toggle navigation"
-              >
-                <span className="navbar-toggler-icon"></span>
-              </button>
-              <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                <ul className="navbar-nav me-auto mb-2 mb-lg-0 w-100">
-                  <li className="nav-item">
-                  <NavLink className="nav-link" aria-current="page" to="/">
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul className="navbar-nav me-auto mb-2 mb-lg-0 w-100">
+              <li className="nav-item">
+                <NavLink className="nav-link" aria-current="page" to="/">
                   Home
                 </NavLink>
               </li>
@@ -56,38 +59,64 @@ function Header() {
                     ? `(${shoppingCartFromStore.length})`
                     : ""}
                 </NavLink>
-                  </li>
-    
-                  <li className="nav-item dropdown">
+              </li>
+              <li className="nav-item">
+                <NavLink
+                  className="nav-link"
+                  aria-current="page"
+                  to="/authentication"
+                >
+                  Authentication
+                </NavLink>
+              </li>
+              <li className="nav-item">
+                <NavLink
+                  className="nav-link"
+                  aria-current="page"
+                  to="/authorization"
+                >
+                  Authorization
+                </NavLink>
+              </li>
+              <li className="nav-item dropdown">
+                <a
+                  className="nav-link dropdown-toggle"
+                  href="https://transform.tools/json-to-typescript"
+                  role="button"
+                  data-bs-toggle="dropdown"
+                  aria-expanded="false"
+                >
+                  Admin Panel
+                </a>
+                <ul className="dropdown-menu">
+                  <li>
                     <a
-                      className="nav-link dropdown-toggle"
+                      className="dropdown-item"
                       href="https://transform.tools/json-to-typescript"
-                      role="button"
-                      data-bs-toggle="dropdown"
-                      aria-expanded="false"
                     >
-                      Admin Panel
+                      Action
                     </a>
-                    <ul className="dropdown-menu">
-                      <li>
-                        <a className="dropdown-item" href="https://transform.tools/json-to-typescript">
-                          Action
-                        </a>
-                      </li>
-                      <li>
-                        <a className="dropdown-item" href="https://transform.tools/json-to-typescript">
-                          Another action
-                        </a>
-                      </li>
-                      <li>
-                        <a className="dropdown-item" href="https://transform.tools/json-to-typescript">
-                          Something else here
-                        </a>
-                      </li>
-                    </ul>
                   </li>
-                  <div className="d-flex" style={{ marginLeft: "auto" }}>
-                  {userData.id && (
+                  <li>
+                    <a
+                      className="dropdown-item"
+                      href="https://transform.tools/json-to-typescript"
+                    >
+                      Another action
+                    </a>
+                  </li>
+                  <li>
+                    <a
+                      className="dropdown-item"
+                      href="https://transform.tools/json-to-typescript"
+                    >
+                      Something else here
+                    </a>
+                  </li>
+                </ul>
+              </li>
+              <div className="d-flex" style={{ marginLeft: "auto" }}>
+                {userData.id && (
                   <>
                     <li className="nav-item">
                       <button
@@ -140,12 +169,12 @@ function Header() {
                   </>
                 )}
               </div>
-                </ul>
-              </div>
-            </div>
-          </nav>
+            </ul>
+          </div>
         </div>
-      );
+      </nav>
+    </div>
+  );
 }
 
 export default Header;
