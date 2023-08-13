20b05fba01d839a05164beb4162c52bfe81d1267
HenokG <heni.gebrehiwot@gmail.com>
2023-08-12 18:23:48 -0600

    Stripe Card Demo

--- src/Pages/Payment.tsx
+++ src/Pages/Payment.tsx
@@ -1,14 +1,26 @@
 import React from "react";
 import { useLocation } from "react-router-dom";
+import { Elements } from "@stripe/react-stripe-js";
+import { loadStripe } from "@stripe/stripe-js";
+import { PaymentForm } from "../Components/Page/Payment";
 
 function Payment() {
   const {
     state: { apiResult, userInput },
   } = useLocation();
+  const stripePromise = loadStripe(
+    "pk_test_51MG6xmDU3OjDrP4GJV8avgtFNNGxikOffHWtYgncDFkahat38KLX3foPPgFbk0JHNG2FmYhIpSNl2lqbnR3uEfRM00Bv0LptTW"
+  );
+  const options = {
+    // passing the client secret obtained from the server
+    clientSecret: apiResult.clientSecret,
+  };
 
-  console.log(apiResult);
-  console.log(userInput);
-  return <div>Payment</div>;
+  return (
+    <Elements stripe={stripePromise} options={options}>
+      <PaymentForm />
+    </Elements>
+  );
 }
 
 export default Payment;
