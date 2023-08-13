9cf82e9194870c3c89df92f9e426ce01c760f44a
HenokG <heni.gebrehiwot@gmail.com>
2023-08-13 12:38:17 -0600

    Order API

--- src/Storage/Redux/store.ts
+++ src/Storage/Redux/store.ts
@@ -1,6 +1,6 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { menuItemReducer } from "./menuItemSlice";
-import { authApi, menuItemApi, paymentApi, shoppingCartApi } from "../../Apis";
+import { authApi, menuItemApi, orderApi, paymentApi, shoppingCartApi } from "../../Apis";
 import { shoppingCartReducer } from "./shoppingCartSlice";
 import { userAuthReducer } from "./userAuthSlice";
 
@@ -13,11 +13,13 @@ const store = configureStore({
     [shoppingCartApi.reducerPath]: shoppingCartApi.reducer,
     [authApi.reducerPath]: authApi.reducer,
     [paymentApi.reducerPath]: paymentApi.reducer,
+    [orderApi.reducerPath]: orderApi.reducer,
   },
   middleware: (getDefaultMiddleware) =>
   getDefaultMiddleware()
   .concat(menuItemApi.middleware)
   .concat(authApi.middleware)
+  .concat(orderApi.middleware)
   .concat(shoppingCartApi.middleware)
   .concat(paymentApi.middleware),
 });
