c9f6da637dcc9b2301b64ce98f0b0249ba3b5bbf
HenokG <heni.gebrehiwot@gmail.com>
2023-08-12 18:13:32 -0600

    UseLocation State

--- src/Pages/Payment.tsx
+++ src/Pages/Payment.tsx
@@ -1,6 +1,13 @@
 import React from "react";
+import { useLocation } from "react-router-dom";
 
 function Payment() {
+  const {
+    state: { apiResult, userInput },
+  } = useLocation();
+
+  console.log(apiResult);
+  console.log(userInput);
   return <div>Payment</div>;
 }
 
