f790617b86f2e06bb34823db72dd402cf9e3e9d6
HenokG <heni.gebrehiwot@gmail.com>
2023-08-15 16:53:25 -0600

    UI for no previous orders

--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -6,6 +6,16 @@ function Home() {
   return (
     <div>
       <Banner />
+      <a
+        href="https://www.linkedin.com/in/henok-gebrehiwot-a9001024b/"
+        target="_blank"
+      >
+        <div className="btn btn-danger form-control text-center text-white h4">
+          This is a demo application developed by Henok using{" "}
+          <span className="text-warning"> ReactJS for Front-End and .Net Web API for Back-End!</span>{" "}
+          To visit my LinkedIn Profile click on me!
+        </div>
+      </a>
       <div className="container p-2">
         <MenuItemList />
       </div>
